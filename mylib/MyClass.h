03d94a79ff072316ae6494fbf0d35ef6164d0272
vbaggiol <vbaggiol@gmail.com>
2018-07-15 12:27:08 +0200

    working CMake example with c-wrapped c++ library

---
+++ mylib/MyClass.h
@@ -0,0 +1,17 @@
+//
+// Created by vbaggiol on 15/07/18.
+//
+
+#ifndef __MYCLASS_H
+#define __MYCLASS_H
+
+class MyClass {
+        private:
+        int m_i;
+        public:
+        void int_set(int i);
+
+        int int_get();
+};
+
+#endif
